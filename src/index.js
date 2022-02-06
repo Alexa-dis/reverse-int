7352701615d95195bf7bbb83e99a189f6be0966a
Safonova Alexandra <alexa-09@bk.ru>
2022-02-06 23:28:41 +0300

    feat: all tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0) {
+       return +(n.toString(10).split('').reverse().join('')) 
+    } else {
+        return +((n * -1).toString(10).split('').reverse().join(''))
+    }
+  ;
 }
